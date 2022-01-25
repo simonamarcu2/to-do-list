02a94afec37fbbd40c254ec78add8e5f57be9a5d
simonamarcu2 <supersimy16@gmail.com>
2022-01-25 15:37:50 +0000

    added buttons , display and form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React { useState } from "react";
+import "./App.css";
+import Form from './components/form'
+import TodoList from "./components/ToDoList";
+import { useState } from "react";
 
-function App() {
+function App(props) {
+  const [inputText, setInputText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>To do</h1>
+        <Form setInputText={setInputText = { setInputText }} />
+        <TodoList />
       </header>
     </div>
-  );
+  )
 }
+
 
 export default App;
